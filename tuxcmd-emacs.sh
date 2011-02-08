f5ef86994b2e5f7456127650b6237eb499a797d7
Mark Vogt <mvogt@src.qcoder.org>
2011-02-07 21:05:37 -0600

    Added GPLv3 file and header comments.

--- tuxcmd-emacs.sh
+++ tuxcmd-emacs.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 #
+# Copyright 2011  Mark Vogt
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
 # Wrapper for emacsclient to run from Tux Commander (tuxcmd).  Due to a bug in
 # Metacity, editing a file with emacsclient won't raise the Emacs window even
 # though it does get the focus.  So, we work around that using wmctrl to raise
