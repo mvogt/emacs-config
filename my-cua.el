afdef50730851fd4898dbe56b1551db2e445220a
Mark Vogt <mvogt@src.qcoder.org>
2011-02-13 18:45:33 -0600

    Tweaks to (still unused) CUA mode.

--- my-cua.el
+++ my-cua.el
@@ -6,10 +6,20 @@
 ;; mapping for vertical scrolling overrides it, and the feature is still handy
 ;; with C-S-right and C-S-left.
 
+;; Don't acutally use the CUA keys.  I want this mode just for the rectangles.
+(setq cua-enable-cua-keys nil)
+
+;; Remap minimize/background to the standard undo key (i.e. swap with undo).
+;;(global-set-key [?\C-/] 'suspend-frame)
+
 ;; Don't let typing replace the selection.
 (setq cua-delete-selection nil)
 
-;; Remap minimize/background to the standard undo key (i.e. swap with undo).
-(global-set-key [?\C-/] 'suspend-frame)
+(setq cua-auto-tabify-rectangles nil)
+
+;; Don't use C-return for rectangle mode because it interferes with some
+;; custom mappings in minor modes that I use.
+(setq cua-rectangle-mark-key [?\C-x ?r ?v])
+(global-set-key [?\C-x ?r ?v] 'cua-set-rectangle-mark)
 
 (cua-mode t)
