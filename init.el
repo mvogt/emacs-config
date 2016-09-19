fa78085f9f38629615ea06abfe84e46d446ed75b
Mark Vogt <mvogt@src.qcoder.org>
2016-09-19 11:40:40 -0500

    Add var package-selected-packages for v25.

--- init.el
+++ init.el
@@ -121,3 +121,12 @@
 ;; between protobuf-mode and cc-mode. That no longer appears to be true, but
 ;; it doesn't hurt for this to be last.
 (load "prg-modes")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet yaml-mode tree-mode rainbow-mode protobuf-mode org magit htmlize hexrgb groovy-mode dropdown-list dired-efap browse-kill-ring))))
