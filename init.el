d341f1d051c6f0ea6e8e957c494515f5c394881a
Mark C. Vogt <mvogt@src.qcoder.org>
2018-06-03 10:17:33 -0500

    Add separate package repo for org mode.

--- init.el
+++ init.el
@@ -94,6 +94,8 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("org" . "https://orgmode.org/elpa/"))
 (package-initialize)
 
 (load "load-save-exit")
